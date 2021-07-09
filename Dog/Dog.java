cfa688cb184e412f0afad21f6fa759c2d22d7d8a
bliutwo <bliutwo@cs.stanford.edu>
2021-07-08 20:45:22 -0700

    Add Dog without testing

---
+++ Dog/Dog.java
@@ -0,0 +1,52 @@
+public class Dog implements DogInterface {
+
+    String name = "";
+    String color = "";
+    String breed = "";
+    boolean hungry = false;
+
+    void setName(String newName) {
+        name = newName;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    void setBreed(String newBreed) {
+        breed = newBreed;
+    }
+
+    String getBreed() {
+        return breed;
+    }
+
+    void setHungerState(boolean newHungerState) {
+        hungry = newHungerState;
+    }
+
+    boolean getHungerState() {
+        return hungry;
+    }
+
+    void bark() {
+        System.out.println(name + " says, BARK!!!");
+    }
+
+    void fetch() {
+        System.out.println(name + " returns to you the stick you threw, in a game of fetch!");
+    }
+
+    void wagTail() {
+        System.out.println(name + " wags its tail.");
+    }
+
+}
---
+++ Dog/DogInterface.java
@@ -0,0 +1,14 @@
+interface DogInterface {
+
+    void setName(String newName);
+    String getName();
+    void setColor(String newColor);
+    String getColor();
+    void setBreed(String newBreed);
+    String getBreed();
+    boolean getHungerState();
+    void bark();
+    void fetch();
+    void wagTail();
+
+}
